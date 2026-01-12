a262f54ab4660b80cde882fefe7e9d0e45d8daeb
kenisha-26 <kenisha1126@gmail.com>
2026-01-12 17:38:04 +0530

    Create prime.c

---
+++ C/prime.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, flag = 0;
+    scanf("%d", &n);
+
+    for(int i = 2; i <= n/2; i++) {
+        if(n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag == 0 && n > 1)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
